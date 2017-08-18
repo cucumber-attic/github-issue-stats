98693df614dd1f2fce7490662ed1cff1054e1d6d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-08-18 12:07:42 +0100

    Fetch issues to disk for analysis

---
+++ README.md
@@ -0,0 +1,4 @@
+## Authentication
+
+   export GITHUB_AUTH='-H "Authorization: token TOKEN"'
+   export GITHUB_AUTH='-u username:password'
---
+++ functions.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+api=https://api.github.com
+
+# Fetches a resource, paginating until the response is empty
+# When all resources are empty, combine them all into one file
+#
+function fetch { # path query
+  path=$1
+  query=$2
+  baseUrl="${api}/${path}?${query}"
+
+  mkdir -p "${path}"
+
+  page=1
+  while :
+  do
+    url="${baseUrl}&page=${page}"
+    echo "${url}"
+    json=$(curl ${GITHUB_AUTH} --fail --silent --show-error "${url}" | jq . -c --raw-output)
+    if [ "${json}" == "[]" ]; then
+      break
+    fi
+    echo "${json}" > "${path}/page-${page}".json
+    page=$((page+1))
+  done
+
+  # Combine them all into one file for easier processing
+  find "${path}" -type f | xargs jq -s add > "${path}.json"
+}
