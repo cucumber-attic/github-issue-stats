f85c889a83a8a988c4370a0a4910f34f42b67f11
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-01-17 10:05:45 +0000

    Quote arguments. Closes #4

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -3,8 +3,8 @@ set -euf -o pipefail
 
 api=https://api.github.com
 
-# Fetches a resource, paginating until the response is empty
-# When all resources are empty, combine them all into one file
+# Fetches a resource, paginating until the response is empty.
+# When all resources have been fetched, combine them all into one file.
 #
 function fetch { # path query
   path=$1
@@ -18,7 +18,7 @@ function fetch { # path query
   do
     url="${baseUrl}&page=${page}"
     echo "${url}"
-    json=$(curl ${GITHUB_AUTH} --fail --silent --show-error "${url}" | jq . -c --raw-output)
+    json=$(curl "${GITHUB_AUTH}" --fail --silent --show-error "${url}" | jq . -c --raw-output)
     if [ "${json}" == "[]" ]; then
       break
     fi
